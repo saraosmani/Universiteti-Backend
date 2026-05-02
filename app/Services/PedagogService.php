afe95c4c630120947d2e1b9778850523e92eb73d
kristinameta <kristinameta@students.uamd.edu.al>
2026-05-02 18:01:15 +0200

    fix edit profile

--- app/Services/PedagogService.php
+++ app/Services/PedagogService.php
@@ -9,31 +9,28 @@ use Illuminate\Validation\ValidationException;
 
 class PedagogService
 {
-
     public function getAllPedagogues()
     {
-        return Pedagog::with('departament')->get();
+        return Pedagog::with("departament")->get();
     }
-
 
     public function getPedagogueById($id)
     {
         return Pedagog::find($id);
     }
 
-
     public function createPedagogue(array $data)
     {
         $validator = Validator::make($data, [
-            'ped_id'         => 'required|string|unique:pedagog,ped_id',
-            'ped_em'         => 'required|string|max:255',
-            'ped_mb'         => 'required|string|max:255',
-            'ped_email'      => 'required|email|unique:pedagog,ped_email',
-            'dep_id'         => 'required|exists:departament,dep_id',
-            'ped_gjin'       => 'nullable|string|max:1',
-            'ped_tit'        => 'nullable|string|max:50',
-            'ped_dl'         => 'nullable|date',
-            'ped_tel'        => 'nullable|string|max:20',
+            "ped_id"    => "required|string|unique:pedagog,ped_id",
+            "ped_em"    => "required|string|max:255",
+            "ped_mb"    => "required|string|max:255",
+            "ped_email" => "required|email|unique:pedagog,ped_email",
+            "dep_id"    => "required|exists:departament,dep_id",
+            "ped_gjin"  => "nullable|string|max:1",
+            "ped_tit"   => "nullable|string|max:50",
+            "ped_dl"    => "nullable|date",
+            "ped_tel"   => "nullable|string|max:20",
         ]);
 
         if ($validator->fails()) {
@@ -43,7 +40,6 @@ class PedagogService
         return Pedagog::create($data);
     }
 
-
     public function updatePedagogue($id, array $data)
     {
         $pedagog = Pedagog::find($id);
@@ -51,19 +47,21 @@ class PedagogService
 
         $pedagog->update($data);
 
-        if (isset($data['ped_em']) || isset($data['ped_mb'])) {
-            $user = User::where('email', $pedagog->ped_email)->first();
-            if ($user) {
-                $user->update([
-                    'name'    => $data['ped_em'] ?? $pedagog->ped_em,
-                    'surname' => $data['ped_mb'] ?? $pedagog->ped_mb,
-                ]);
+        $user = User::find($pedagog->user_id);
+
+        if ($user) {
+            $userUpdate = [];
+            if (isset($data["ped_em"]))    $userUpdate["name"]    = $data["ped_em"];
+            if (isset($data["ped_mb"]))    $userUpdate["surname"] = $data["ped_mb"];
+            if (isset($data["ped_email"])) $userUpdate["email"]   = $data["ped_email"];
+
+            if (!empty($userUpdate)) {
+                $user->update($userUpdate);
             }
         }
 
         return $pedagog;
     }
-
 
     public function deletePedagogue($id)
     {
